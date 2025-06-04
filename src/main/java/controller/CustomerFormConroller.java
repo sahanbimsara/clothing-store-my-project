2467515e533d95b0ef8de065ff5698c7d1b8ed3b
sahanbimsara <sahanbimsara2060@gmail.com>
2025-06-04 16:30:13 +0530

    first commit

---
+++ src/main/java/controller/CustomerFormConroller.java
@@ -0,0 +1,42 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CustomerFormConroller {
+
+    @FXML
+    private TableColumn colAddress;
+
+    @FXML
+    private TableColumn colName;
+
+    @FXML
+    private TableColumn colPhone;
+
+    @FXML
+    private TextField txtAddress;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPhone;
+
+    @FXML
+    private TableView txtTable;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+}
